1ec6419eae5e3bd4ddca28544f76d23ad0d66735
Shinpei Hayashi <hayashi@c.titech.ac.jp>
2021-09-20 10:40:12 +0900

    Drop import statements in MPASplitter

--- src/main/java/jp/ac/titech/c/phph/parse/MPASplitter.java
+++ src/main/java/jp/ac/titech/c/phph/parse/MPASplitter.java
@@ -5,6 +5,7 @@ import jp.ac.titech.c.phph.model.Statement;
 import yoshikihigo.cpanalyzer.CPAConfig;
 import yoshikihigo.cpanalyzer.LANGUAGE;
 import yoshikihigo.cpanalyzer.StringUtility;
+import yoshikihigo.cpanalyzer.lexer.token.IMPORT;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -21,6 +22,7 @@ public class MPASplitter implements Splitter {
     public List<Statement> split(String source) {
         return StringUtility.splitToStatements(source, LANGUAGE.JAVA)
                 .stream()
+                .filter(s -> !(s.tokens.get(0) instanceof IMPORT)) // drop import statements
                 .map(this::convert)
                 .collect(Collectors.toList());
     }
