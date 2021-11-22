891c82c9c110df97f611ac191aed966f4aa9e3b0
Shinpei Hayashi <hayashi@c.titech.ac.jp>
2021-11-22 13:54:19 +0900

    Make the inspection format better

--- src/main/java/jp/ac/titech/c/phph/cmd/ShowCommand.java
+++ src/main/java/jp/ac/titech/c/phph/cmd/ShowCommand.java
@@ -1,5 +1,6 @@
 package jp.ac.titech.c.phph.cmd;
 
+import com.google.common.collect.Lists;
 import jp.ac.titech.c.phph.db.Dao;
 import jp.ac.titech.c.phph.model.Fragment;
 import jp.ac.titech.c.phph.model.Match;
@@ -48,18 +49,20 @@ public class ShowCommand extends BaseCommand {
         final String like = prefix + "%";
         final int count = dao.countFragments(like);
         log.debug("Search fragments for '{}', {} found", like, count);
-        if (count == 1) {
-            final Fragment f = dao.searchFragments(like).findFirst().get();
-            System.out.printf("Hash: %s\n", f.getHash());
-            System.out.printf("Text:\n------\n%s\n------\n", f.getText());
-            System.out.println("Pattern:");
-            for (final Pattern p : dao.listPatterns(f)) {
-                System.out.println(p);
-            }
-        } else {
+        if (count > 1) {
             for (final Fragment f : dao.searchFragments(like)) {
                 System.out.println(f.getHash());
             }
+            return;
+        }
+
+        final Fragment f = dao.searchFragments(like).findFirst().get();
+        System.out.printf("Hash: %s\n", f.getHash());
+        System.out.printf("Text:\n%s\n\n", f.getText());
+        final List<Pattern> patterns = Lists.newArrayList(dao.listPatterns(f));
+        System.out.printf("Patterns (%d):\n", patterns.size());
+        for (final Pattern p : patterns) {
+            System.out.println(p);
         }
     }
 
@@ -82,24 +85,25 @@ public class ShowCommand extends BaseCommand {
 
         final Pattern p = dao.searchPatterns(like).findFirst().get();
         System.out.printf("Hash: %s\n", p.getHash());
-        System.out.printf("------\n%s\n------\n%s\n------\n", dao.findFragment(p.getOldHash()).get().getText(),
-                                                              dao.findFragment(p.getNewHash()).get().getText());
-        System.out.println("Chunk:");
-        int cid = -1;
-        Dao.Commit commit = null;
-        final String repository = dao.findRepository().get();
-        for (final Dao.DBChunk h : dao.listChunks(p.getHash())) {
-            if (cid != h.getCommitId()) {
-                cid = h.getCommitId();
-                commit = dao.findCommit(cid).get();
-                System.out.printf("Commit %s - %s\n", commit.getHash(), commit.getMessage());
-            }
-            System.out.printf("--- %s:%s\n", h.getFile(), h.getOldLines());
-            System.out.printf("+++ %s:%s\n", h.getFile(), h.getNewLines());
-            System.out.println(inspectChunk(repository, commit.getHash(), h));
-        }
+        System.out.printf("Pattern:\n%s\n-->\n%s\n\n", dao.findFragment(p.getOldHash()).get().getText(),
+                                                       dao.findFragment(p.getNewHash()).get().getText());
 
-        System.out.println("Matches:");
+        final String repository = dao.findRepository().get();
+
+        final List<Dao.DBChunk> chunks = Lists.newArrayList(dao.listChunks(p.getHash()));
+        System.out.printf("Chunks (%d):\n", chunks.size());
+        chunks.stream().collect(Collectors.groupingBy(Dao.DBChunk::getCommitId)).forEach((cid, hs) -> {
+            final Dao.Commit commit = dao.findCommit(cid).get();
+            System.out.printf("Commit %s: %s\n", commit.getHash(), commit.getMessage());
+            for (final Dao.DBChunk h : hs) {
+                System.out.printf("--- %s:%s @ %s\n", h.getFile(), h.getOldLines(), commit.getHash());
+                System.out.printf("+++ %s:%s @ %s\n", h.getFile(), h.getNewLines(), commit.getHash());
+                System.out.println(inspectChunk(repository, commit.getHash(), h));
+            }
+        });
+
+        final List<Match> matches = Lists.newArrayList(dao.listMatches(p.getOldHash()));
+        System.out.printf("\nMatches (%d):\n", matches.size());
         for (final Match m : dao.listMatches(p.getOldHash())) {
             System.out.println(inspectMatch(repository, "HEAD", m));
         }
