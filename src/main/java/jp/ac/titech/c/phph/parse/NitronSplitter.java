b838c1e711773f7e7f3c5920258ad0674dbc25e2
Shinpei Hayashi <hayashi@c.titech.ac.jp>
2022-12-22 12:44:50 +0900

    Continuously runs even when parsing error occurs

--- src/main/java/jp/ac/titech/c/phph/parse/NitronSplitter.java
+++ src/main/java/jp/ac/titech/c/phph/parse/NitronSplitter.java
@@ -1,5 +1,6 @@
 package jp.ac.titech.c.phph.parse;
 
+import com.github.durun.nitron.core.ParsingException;
 import com.github.durun.nitron.core.ast.node.AstNode;
 import com.github.durun.nitron.core.ast.processors.AstProcessor;
 import com.github.durun.nitron.core.ast.processors.AstSplitter;
@@ -10,8 +11,10 @@ import com.github.durun.nitron.core.config.LangConfig;
 import com.github.durun.nitron.core.parser.NitronParser;
 import jp.ac.titech.c.phph.model.Range;
 import jp.ac.titech.c.phph.model.Statement;
+import lombok.extern.slf4j.Slf4j;
 
 import java.io.StringReader;
+import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
@@ -19,6 +22,7 @@ import java.util.stream.Collectors;
 /**
  * The splitter using nitron.
  */
+@Slf4j
 public class NitronSplitter implements Splitter {
     private final NitronParser parser;
     private final AstSplitter splitter;
@@ -41,12 +45,17 @@ public class NitronSplitter implements Splitter {
 
     @Override
     public List<Statement> split(final String source) {
-        final AstNode root = parser.parse(new StringReader(source));
-        return splitter.process(root)
-                .stream()
-                .map(this::normalizeAndConvert)
-                .filter(Objects::nonNull)
-                .collect(Collectors.toList());
+        try {
+            final AstNode root = parser.parse(new StringReader(source));
+            return splitter.process(root)
+                    .stream()
+                    .map(this::normalizeAndConvert)
+                    .filter(Objects::nonNull)
+                    .collect(Collectors.toList());
+        } catch (final ParsingException e) {
+            log.error(e.getMessage());
+            return Collections.emptyList();
+        }
     }
 
     private Statement normalizeAndConvert(final AstNode node) {
